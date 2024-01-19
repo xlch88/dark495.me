0535d34554698666c182390c4a63e9e64f66fa96
悦咚 <flandrestudio.cn@gmail.com>
2024-01-19 23:04:17 +0800

    goods

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 当前的Dark495.me主页所使用的源代码，收集了一些观点和**批话**。
 
 https://dark495.me/
+
+## good.php
+参数使用实例：
+
+https://dark495.me/good.php?w=%E5%96%B5&bc=b7e2fb&brc=fff&wc=fff&bc=b7e2fb&wbc=linear-gradient(135deg,rgb(255,169,187),rgb(148,179,233))
+
+- `w` : 字，或者在`$allowWords`里的词/短语。
+- `bc` : 背景颜色
+- `wbc` : 文本框背景颜色
+- `brc` : 文本框边框颜色
+- `wc` : 文本颜色
+
+其中16进制颜色代码不需要加前面的`#`号，支持传入函数(如示例链接的渐变)
--- good.php
+++ good.php
@@ -1,5 +1,11 @@
 <?php
-$word = in_array($_GET['w'] ?? '', ['感恩']) || mb_strlen($_GET['w'] ?? '', 'UTF-8') == 1 ? $_GET['w'] : '好';
+$allowWords = [
+	'感恩',
+	'社会主义好',
+];
+
+$_GET = array_map(fn($w) => htmlspecialchars($w), $_GET);
+$word = in_array($_GET['w'] ?? '', $allowWords) || mb_strlen($_GET['w'] ?? '', 'UTF-8') == 1 ? $_GET['w'] : '好';
 $exp = isset($_GET['e']) ? (is_array($_GET['e']) ? $_GET['e'] : [$_GET['e']]) : ['，', '。'];
 $exp = count(array_filter($exp, function($v){ return mb_strlen($v, 'UTF-8') > 1; })) > 0 ? ['，', '。'] : $exp;
 $end = isset($_GET['d']) && mb_strlen($_GET['d'], 'UTF-8') == 1 ? $_GET['d'] : '。';
@@ -9,6 +15,18 @@ function e($t, $c) {
 		echo $t;
 	}
 }
+
+function c($name, $default){
+	if(!isset($_GET[$name]) || $_GET[$name] == ''){
+		return $default;
+	}
+	
+	if(preg_match('/^[0-9a-f]{1,8}$/i', $_GET[$name])){
+		return '#' . $_GET[$name];
+	}
+	
+	return $_GET[$name];
+}
 ?>
 <html>
 	<head>
@@ -17,10 +35,38 @@ function e($t, $c) {
 		<title><?=e($word, rand(3, 20)); ?></title>
 		
 		<style>
-		body{ background: #f00; margin: 15px; }
-		.good{ max-width: 1000px; margin:0 auto; background: #000; border: 5px solid #00f; padding: 20px; color:#0f0; }
-		h1{ text-align: center; margin-top:0; }
-		p{ text-indent: 2em; word-break: break-all; }
+		body{
+			background: <?=c('bc', '#f00'); ?>;
+			margin: 15px;
+		}
+		.good{
+			max-width: 1000px;
+			margin:0 auto;
+			background: <?=c('wbc', '#000'); ?>;
+			border: 5px solid <?=c('brc', '#00f'); ?>;
+			padding: 20px;
+			color: <?=c('wc', '#0f0'); ?>;
+		}
+		h1{
+			text-align: center;
+			margin-top:0;
+		}
+		p{
+			text-indent: 2em;
+			word-break: break-all;
+		}
+		
+		.text-center{
+			text-align: center;
+		}
+		
+		a{
+			color: #fff;
+			text-shadow: 0 0 10px #000;
+			text-decoration: none;
+			margin: 20px 0;
+			display: block;
+		}
 		</style>
 	</head>
 	<body>
@@ -29,6 +75,10 @@ function e($t, $c) {
 			<?php for($x = 0; $x < rand(50, 100); $x++){ ?>
 			<p><?php for($y = 0; $y < rand(10, 20); $y++){ echo e($word, rand(10, 20), ); echo $exp[rand(0, count($exp) - 1)]; } echo e($word, rand(3, 20)) . $end; ?></p>
 			<?php } ?>
+		</div>
+		
+		<div class="text-center">
+			<a href="https://dark495.me/">Dark495.me</a>
 		</div>
 	</body>
 </html>
